d45d2abc2de9fb448a2c76d1ea5d4348c428a450
lmarkov <lmarkov.webmaster@ya.ru>
2022-01-03 16:37:35 +0300

    add tailwind css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap";
+import "./index.css";
 
 createApp(App).mount("#app");
