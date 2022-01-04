27b8ef4904b42c8a27d04c015400fcf161812f1f
lmarkov <lmarkov.webmaster@ya.ru>
2022-01-04 19:32:35 +0300

    add new feature math components

---
+++ src/Components/Math/AppConversion.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Конвертации. Конвертация $ -> Р.</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
---
+++ src/Components/Math/AppPercentage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Процент от числа, На сколько процентов одно число больше другого</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
