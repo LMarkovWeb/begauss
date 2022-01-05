82b4cc13f93ed0a84b329795f1e6d901ba626cce
lmarkov <lmarkov.webmaster@ya.ru>
2022-01-05 12:02:11 +0300

    add new pages for feature components

---
+++ src/views/AllTasks.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <task-section :levelsConfig="levelsConfig" />
+  </div>
+</template>
+
+<script>
+import TaskSection from "@/Components/TaskSection.vue";
+export default {
+  components: { TaskSection },
+  props: ["levelsConfig"],
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Middle.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Middle</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
